97222c032f6bc2b592edf5ec40a356669079c631
dass2608 <jarvisseekamp@gmail.com>
2023-12-28 16:06:02 +0100

    Levels 19 and 20
    Added Levels 19 and 20.
    Also fixed the DirectionPad EditorScript not updating the apperance correctly

--- src/scripts/DirectionPad.gd
+++ src/scripts/DirectionPad.gd
@@ -1,14 +1,20 @@
 @tool
 extends Node2D
 
-@export var direction:Directions
+@export var direction:Directions:
+	set(new_value):
+		direction = new_value
+		_ready()
 ## The power of the applied impulse. THis will bw multiplied by 100 to be used as impulse
 @export var strength:float = 1:
 	set(new_value):
 		strength = new_value
 		update_configuration_warnings()
 ## If enabled, any motion the player has will be nullified before the impulse is applied
-@export var nullifySpeed:bool = false
+@export var nullifySpeed:bool = false:
+	set(new_value):
+		nullifySpeed = new_value
+		_ready()
 
 var impulse:Vector2
 const IMPULSE_MULTIPLIER:int = 100
@@ -35,15 +41,17 @@ func _ready():
 			$StaticBody2D.rotation = deg_to_rad(0)
 		Directions.DOWN:
 			impulse = Vector2(0, strength * IMPULSE_MULTIPLIER)
-			$StaticBody2D.rotation += deg_to_rad(180)
+			$StaticBody2D.rotation = deg_to_rad(180)
 		Directions.LEFT:
 			impulse = Vector2(-strength * IMPULSE_MULTIPLIER, 0)
-			$StaticBody2D.rotation += deg_to_rad(-90)
+			$StaticBody2D.rotation = deg_to_rad(-90)
 		Directions.RIGHT:
 			impulse = Vector2(strength * IMPULSE_MULTIPLIER, 0)
-			$StaticBody2D.rotation += deg_to_rad(90)
+			$StaticBody2D.rotation = deg_to_rad(90)
 	if nullifySpeed:
 		$StaticBody2D/Sprite2D.modulate = Color("ff8bff")
+	else:
+		$StaticBody2D/Sprite2D.modulate = Color("ffffff")
 
 func _on_static_body_2d_body_entered(body:RigidBody2D):
 	if nullifySpeed:
